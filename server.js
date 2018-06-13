faf7a392a15ce04a25266a5e1e652a616fb7d2d0
saikrishna321 <saikrishna321@yahoo.com>
2018-06-13 15:22:39 +0100

    add chalk to color console

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-import http from "http";
-import app from "./index";
-import dbHelpers from "./db";
+#!/usr/bin/env node
+import http from 'http';
+import app from './index';
+import dbHelpers from './db';
+import chalk from 'chalk';
 
 const port = process.env.PORT || 3000;
 const mongoPort = process.env.MONGOPORT || 27017;
 
-dbHelpers.connect("mongodb://localhost:" + mongoPort + "/report", err => {
-  if (err) {
-    console.log("Unable to connect to Mongo.");
-    process.exit(1);
-  } else {
-    app.listen(port, function() {
-      console.log("Listening on port " + port + "...");
-    });
+dbHelpers.connect(
+  'mongodb://localhost:' + mongoPort + '/report',
+  err => {
+    if (err) {
+      console.log(chalk.blue('Unable to connect to Mongo.'));
+      process.exit(1);
+    } else {
+      app.listen(port, function() {
+        console.log(chalk.blue('Listening on port ' + port + '...'));
+      });
+    }
   }
-});
+);
