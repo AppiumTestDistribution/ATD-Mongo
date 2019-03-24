45b7bb08553281f431a274cbb0b490007b43df52
saikrishna321 <saikrishna321@yahoo.com>
2019-03-24 17:23:39 +0000

    configure mongo host

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ import app from './index';
 import dbHelpers from './db';
 import chalk from 'chalk';
 
-const port = process.env.PORT || 3000;
+const port = process.env.MONGOAPIPORT || 3000;
 const mongoPort = process.env.MONGOPORT || 27017;
+const mongoIP = process.env.MONGOIP || 'localhost';
 
 dbHelpers.connect(
-  'mongodb://localhost:' + mongoPort + '/report',
-  err => {
+  `mongodb://${mongoIP}:${mongoPort}/report`,
+   err => {
     if (err) {
-      console.log(chalk.blue('Unable to connect to Mongo.'));
+      console.log(chalk.blue(`Unable to connect to Mongo ${err}`));
       process.exit(1);
     } else {
       app.listen(port, function() {
-        console.log(chalk.blue('Listening on port ' + port + '...'));
+        console.log(chalk.blue(`Listening on port ${mongoPort} and IP ${mongoIP}`));
       });
     }
   }
